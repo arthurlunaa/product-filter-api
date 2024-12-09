dccc7bbf4d7e5d29d7525d60de10b309c337166e
arthurlunaa <henriquearthur192@gmail.com>
2024-12-09 19:55:58 -0300

    feat:create: produto-filter-api

---
+++ src/scripts/script.js
@@ -0,0 +1,31 @@
+// decl
+const productList = document.getElementById ('product-list')
+const filterInput = document.getElementById('filter-input')
+let products = []
+//  consumo da api 
+async function fetchProducts() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        products = await response.json()
+        
+    } catch (error){
+        productList.innerHTML = `<p> Erro ao carregar os produtos . tente novamente  ${error}</p>`
+    }
+    
+    
+}
+// função para exibir produto 
+function displayProducts(filteredProducts){
+    productList.innerHTML = ''
+    if (filteredProducts.lenght ===0) {
+          productList.innerHTML ='<p> nenhum produto encontrado   </p>'
+    }
+
+    filteredProducts.array.forEach((products) => {
+        //criando uma div com js 
+        const productElement = document.createElement('div')
+        // adicionando uma classe com js
+        productElement.className('product')
+    });
+
+}
